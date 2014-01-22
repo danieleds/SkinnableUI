15d7fd25ce7e200793c3f0e6407b23bc0c38c334
Daniele Di Sarli <danieleds0@gmail.com>
2014-01-22 15:11:30 +0100

    Rinominati assembly

--- SkinDesigner/Properties/AssemblyInfo.cs
+++ SkinDesigner/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UnusefulPlayer")]
+[assembly: AssemblyTitle("SkinDesigner")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnusefulPlayer")]
+[assembly: AssemblyProduct("SkinDesigner")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
