7e3f0a072b08781393e7fa1406a94942a80913a9
Daniele Di Sarli <danieleds0@gmail.com>
2014-02-09 12:37:36 +0100

    Added copyright and license notice

--- SkinDesigner/frmSkinPreview.cs
+++ SkinDesigner/frmSkinPreview.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿/*
+ *  Copyright 2014 Daniele Di Sarli
+ *
+ *  This file is part of SkinnableUI.
+ *
+ *  SkinnableUI is free software: you can redistribute it and/or modify
+ *  it under the terms of the Lesser GNU General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  SkinnableUI is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  Lesser GNU General Public License for more details.
+ *
+ *  You should have received a copy of the Lesser GNU General Public License
+ *  along with SkinnableUI. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
